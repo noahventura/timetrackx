c0a15ff69d0f323b62b379521115f456fa3a6fc8
xMerkery <mw3.noah@gmail.com>
2023-11-26 13:34:12 -0500

    File structure for login and about, redirect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
+import logo from './images/logo.png'
 import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './components/about/About' // Your page component
+import Login from './components/login/Login'; // Your page component
 function App() {
   return (
+    <Router>
     <div>
-      <h1>Hello world!</h1>
-    <Button variant='outlined'>This is a button</Button>
+      <div id='navBar'>
+        {/* <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About Us</Link>
+            </li>
+            <li>
+              <Link to='/login'>login</Link>
+            </li>
+          </ul>
+        </nav> */}
+      </div>
+      
+      <img src={logo} alt="Timetrackx logo" />
     </div>
+    
+<Routes>
+  <Route path='/about' element={<About />} />
+  <Route path='/login' element={<Login />} />
+</Routes>
+    </Router>
+
   );
 }
 
