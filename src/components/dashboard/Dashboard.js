447307f5abdb4ed18b445e1f8ec7c7f8b63922c5
xMerkery <mw3.noah@gmail.com>
2023-11-29 23:58:34 -0500

    draggable tasks

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -3,51 +3,83 @@ import  './styles/styles.css'
 
 class Dashboard extends Component {
     constructor(props) {
-      super(props);
-      // Initialize state with an empty panels array
-      this.state = {
-        panels: []
-      };
+        super(props);
+        this.state = {
+            panels: []
+        };
     }
-  
+
     addPanel = () => {
-      // Add a new panel with an empty tasks array
-      this.setState(prevState => ({
-        panels: [...prevState.panels, { tasks: [] }]
-      }));
+        this.setState(prevState => ({
+            panels: [...prevState.panels, { tasks: [] }]
+        }));
     }
-  
+
     addTask = (panelIndex, taskDescription) => {
-      // Add a new task to the specified panel
-      const newPanels = this.state.panels.map((panel, index) => {
-        if (index === panelIndex) {
-          return { ...panel, tasks: [...panel.tasks, taskDescription] };
+        const newPanels = this.state.panels.map((panel, index) => {
+            if (index === panelIndex) {
+                return { ...panel, tasks: [...panel.tasks, taskDescription] };
+            }
+            return panel;
+        });
+
+        this.setState({ panels: newPanels });
+    }
+
+    onDragStart = (event, task, panelIndex) => {
+        event.dataTransfer.setData("task", task);
+        event.dataTransfer.setData("fromPanel", panelIndex);
+    }
+
+    onDragOver = (event) => {
+        event.preventDefault();
+    }
+
+    onDrop = (event, toPanelIndex) => {
+        const task = event.dataTransfer.getData("task");
+        const fromPanelIndex = event.dataTransfer.getData("fromPanel");
+
+        if (fromPanelIndex !== toPanelIndex) {
+            this.moveTask(task, fromPanelIndex, toPanelIndex);
         }
-        return panel;
-      });
-  
-      this.setState({ panels: newPanels });
     }
-  
+
+    moveTask = (task, fromPanelIndex, toPanelIndex) => {
+        this.setState(prevState => {
+            let newPanels = prevState.panels.map(panel => ({ ...panel, tasks: [...panel.tasks] }));
+    
+            // Find and remove the task from the original panel
+            newPanels[fromPanelIndex].tasks = newPanels[fromPanelIndex].tasks.filter(t => t !== task);
+    
+            // Add the task to the new panel
+            newPanels[toPanelIndex].tasks.push(task);
+    
+            return { panels: newPanels };
+        });
+    }
+    
+
     render() {
-      return (
-        <div id='fullDash'>
-          <button onClick={this.addPanel}>Add Panel</button>
-          <div className='panels'>
-            {this.state.panels.map((panel, panelIndex) => (
-              <div key={panelIndex} className="panel">
-                <button onClick={() => this.addTask(panelIndex, 'New Task')}>
-                  Add Task
-                </button>
-                {panel.tasks.map((task, taskIndex) => (
-                  <div key={taskIndex}>{task}</div>
-                ))}
-              </div>
-            ))}
-          </div>
-        </div>
-      );
+        return (
+            <div id='fullDash'>
+                <button onClick={this.addPanel}>Add Panel</button>
+                <div className='panels'>
+                    {this.state.panels.map((panel, panelIndex) => (
+                        <div key={panelIndex} className="panel" onDragOver={this.onDragOver} onDrop={(event) => this.onDrop(event, panelIndex)}>
+                            <button onClick={() => this.addTask(panelIndex, 'New Task')}>
+                                Add Task
+                            </button>
+                            {panel.tasks.map((task, taskIndex) => (
+                                <div key={taskIndex} draggable onDragStart={(event) => this.onDragStart(event, task, panelIndex)}>
+                                    {task}
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
     }
-  }
-  
-  export default Dashboard;
+}
+
+export default Dashboard;
