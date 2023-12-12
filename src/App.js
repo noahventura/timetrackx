d96370238cc58853df684f0577234bede2da8b4c
xMerkery <mw3.noah@gmail.com>
2023-12-11 23:54:22 -0500

    materials imported and styles declared in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import logo from './images/logo.png'
 import { Button } from '@mui/material';
-import { Link } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './components/about/About' // Your page component
 import Login from './components/login/Login'; // Your page component
 import Dashboard from './components/dashboard/Dashboard';
+import { AppBar, Toolbar, Typography, Button, makeStyles } from '@mui/material';
+import { Link as RouterLink, useLocation } from 'react-router-dom';
+
+//MATERIAL STYLES
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    marginBottom: theme.spacing(4), // Adds space below the navbar
+  },
+  title: {
+    flexGrow: 1, // Makes the title take up the full space to push navigation to the right
+  },
+  link: {
+    color: theme.palette.common.white,
+    textDecoration: 'none',
+    marginRight: theme.spacing(2),
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
+  activeLink: {
+    textDecoration: 'underline',
+  },
+}));
+
+
+
+
+
 function App() {
   return (
     <Router>
